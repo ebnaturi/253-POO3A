f1d19e3aa2172b71fe11c1bd9df11cd4d1c2c9ad
progC_7A <nathury@hotmail.com>
2025-11-05 13:11:15 -0600

    terminado usuario

--- src/main/java/org/ej3b/controllers/MateriaController.java
+++ src/main/java/org/ej3b/controllers/MateriaController.java
@@ -7,7 +7,7 @@ import org.ej3b.services.MateriaService;
 import java.util.List;
 
 import io.javalin.http.Context;
-
+import io.javalin.http.HttpStatus;
 import java.sql.SQLException;
 
 import com.password4j.Password;
@@ -61,15 +61,14 @@ public class MateriaController {
             ctx.status(500).result("error al guardar la materia");
         }
     }
-    /*
     //esto no se debe de hacer solo es para prueba
     public void testUser(Context ctx){
         Usuarios ur = ctx.bodyAsClass(Usuarios.class);
 
-        String hasPass = Password.hash(ur.getPass()).withBcrypt().getResult();
-        System.out.println("EL usuario :" + ur.getUser()+" tiene el hash "+hasPass);
+        String hasPass = Password.hash(ur.getPwsd()).withBcrypt().getResult();
+        System.out.println("EL usuario :" + ur.getNom()+" tiene el hash "+hasPass);
     }
-
+    /*
     public void loginUser(Context ctx){
         UsuarioRegistro ur = ctx.bodyAsClass(UsuarioRegistro.class);
         try{
